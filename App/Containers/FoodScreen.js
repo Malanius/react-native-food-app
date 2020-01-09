26f349d0c80293eaa3a1c89338fcc3f71e8099c0
Malanius Privierre <malaniusprivierre@gmail.com>
2020-01-09 13:31:06 +0100

    Style the title on FoodScreen

--- App/Containers/FoodScreen.js
+++ App/Containers/FoodScreen.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { View } from 'react-native'
+import { Container, Content } from 'native-base'
 
 import HeaderFood from '../Components/HeaderFood'
-import styles from './Styles/LaunchScreenStyles'
+import styles from './Styles/FoodScreenStyles'
 import { Text } from 'native-base'
 
 const foodScreen = (props) => (
 
-    <View style={styles.mainContainer}>
+    <Container>
         <HeaderFood navigate={props.navigation} />
-        <View>
-            <Text>{props.navigation.getParam('title', '')}</Text>
-        </View>
-    </View>
+        <Content style={styles.content}>
+            <Text style={styles.title}>{props.navigation.getParam('title', '')}</Text>
+        </Content>
+    </Container>
 
 )
 
--- App/Containers/Styles/FoodScreenStyles.js
+++ App/Containers/Styles/FoodScreenStyles.js
@@ -1,5 +1,12 @@
 import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
-
+    content: {
+        marginTop: 18
+    },
+    title: {
+        fontSize: 24,
+        textAlign: 'center',
+        fontWeight: "600"
+    }
 })
