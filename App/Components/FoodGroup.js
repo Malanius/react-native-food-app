91b5a4934236ab1d7896d40df38072b73b80575e
Malanius Privierre <malaniusprivierre@gmail.com>
2020-01-10 16:01:09 +0100

    Use goTo navigation methods in FoodGroup

--- App/Components/FoodGroup.js
+++ App/Components/FoodGroup.js
@@ -2,18 +2,11 @@ import React from 'react'
 import { View, Image, Text, TouchableOpacity } from 'react-native'
 
 
-import { burgers, chicken, pizza } from '../Components/Menu'
+
 // Styles
 import styles from './Styles/FoodGroupStyles'
 
 const foodGroup = (props) => {
-
-    let items = burgers;
-    if (props.text === "CHICKEN"){
-        items = chicken;
-    }if (props.text === "PIZZA"){
-        items = pizza;
-    }
 
     return (
         <View style={styles.foodCard}>
@@ -24,10 +17,7 @@ const foodGroup = (props) => {
                     blurRadius={3} />
             </View>
             <View style={styles.textView}>
-                <TouchableOpacity onPress={() => props.navigate('FoodScreen', {
-                    title: props.text,
-                    items: items
-                })}>
+                <TouchableOpacity onPress={props.goTo}>
                     <Text style={styles.text}>{props.text}</Text>
                 </TouchableOpacity>
             </View>
