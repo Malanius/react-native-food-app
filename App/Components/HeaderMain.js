12af8e2f39e61610886bc30574db2863ea3ad66a
Malanius Privierre <malaniusprivierre@gmail.com>
2020-01-10 16:00:51 +0100

    Create drawer menu

--- App/Components/HeaderMain.js
+++ App/Components/HeaderMain.js
@@ -5,12 +5,12 @@ import { Container, Header, Left, Body, Right, Button, Icon } from 'native-base'
 // Styles
 import styles from './Styles/HeaderMainStyles'
 
-const headerLanding = () => (
+const headerLanding = (props) => (
 
   <Container style={styles.container}>
     <Header style={styles.header}>
       <Left>
-        <Button transparent onPress={() => alert("Menu button pressed!")}>
+        <Button transparent onPress={props.openDrawer}>
           <Icon name="menu" style={styles.icon} />
         </Button>
       </Left>
--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
 import { ScrollView, View } from 'react-native'
+import { Drawer } from 'native-base'
 
 import HeaderLanding from '../Components/HeaderMain'
 import FoodGroup from '../Components/FoodGroup'
+import Sidebar from '../Components/Sidebar'
+
+import { burgers, chicken, pizza, coldDrinks, hotBevarages } from '../Components/Menu'
 
 // Styles
 import styles from './Styles/LaunchScreenStyles'
@@ -14,17 +18,78 @@ const pizzaImage = require('../Images/food/pizza.jpg')
 
 export default class LaunchScreen extends Component {
 
+
+  closeDrawer = () => {
+    this.drawer._root.close();
+  }
+
+  openDrawer = () => {
+    this.drawer._root.open();
+  }
+
+  goToBurgers = () => {
+    this.props.navigation.navigate('FoodScreen', {
+      title: 'BURGERS',
+      items: burgers
+    });
+    this.closeDrawer();
+  }
+
+  goToChicken = () => {
+    this.props.navigation.navigate('FoodScreen', {
+      title: 'CHICKEN',
+      items: chicken
+    });
+    this.closeDrawer();
+  }
+
+  goToPizza = () => {
+    this.props.navigation.navigate('FoodScreen', {
+      title: 'PIZZA',
+      items: pizza
+    });
+    this.closeDrawer();
+  }
+
+  goToColdDrinks = () => {
+    this.props.navigation.navigate('FoodScreen', {
+      title: 'COLD DRINKS',
+      items: coldDrinks
+    });
+    this.closeDrawer();
+  }
+
+  goToHotBevarages = () => {
+    this.props.navigation.navigate('FoodScreen', {
+      title: 'HOT BEVARAGES',
+      items: hotBevarages
+    });
+    this.closeDrawer();
+  }
+
   render() {
-    const { navigate } = this.props.navigation
+
     return (
-      <View style={styles.mainContainer}>
-        <HeaderLanding />
-        <ScrollView style={styles.scroll}>
-          <FoodGroup image={burgerImage} text="BURGERS" navigate={navigate} />
-          <FoodGroup image={chickenImage} text="CHICKEN" navigate={navigate} />
-          <FoodGroup image={pizzaImage} text="PIZZA" navigate={navigate} />
-        </ScrollView>
-      </View>
+      <Drawer ref={(ref) => this.drawer = ref}
+        content={<Sidebar
+          goToBurgers={this.goToBurgers}
+          goToChicken={this.goToChicken}
+          goToPizza={this.goToPizza}
+          goToColdDrinks={this.goToColdDrinks}
+          goToHotBevarages={this.goToHotBevarages}
+        />}
+        onClose={() => this.closeDrawer()}
+      >
+
+        <View style={styles.mainContainer}>
+          <HeaderLanding openDrawer={this.openDrawer} />
+          <ScrollView style={styles.scroll}>
+            <FoodGroup image={burgerImage} text="BURGERS" goTo={this.goToBurgers} />
+            <FoodGroup image={chickenImage} text="CHICKEN" goTo={this.goToChicken} />
+            <FoodGroup image={pizzaImage} text="PIZZA" goTo={this.goToPizza} />
+          </ScrollView>
+        </View>
+      </Drawer>
     )
   }
 }
