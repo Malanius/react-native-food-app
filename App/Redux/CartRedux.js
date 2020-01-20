00b0759bbb3b207d85c4c1a19b471068ce615427
Malanius Privierre <malaniusprivierre@gmail.com>
2020-01-20 13:57:27 +0100

    Prepare cart redux

---
+++ App/Redux/CartRedux.js
@@ -0,0 +1,78 @@
+import { createReducer, createActions } from 'reduxsauce'
+import Immutable from 'seamless-immutable'
+
+/* ------------- Types and Action Creators ------------- */
+
+const { Types, Creators } = createActions({
+  changeCart: ['cart'],
+  changeItemcountup: ['cart'],
+  changeItemcountdown: ['cart'],
+  changeRemoveitem: ['cart'],
+  changeClearcart: ['cart']
+})
+
+export const CartTypes = Types
+export default Creators
+
+/* ------------- Initial State ------------- */
+
+export const INITIAL_STATE = Immutable({
+  cart: []
+})
+
+/* ------------- Reducers ------------- */
+
+export const changeCart = (state, { cart }) => {
+  const cartArray = [...state.cart];
+  cartArray.push(cart);
+  return state.merge({ cart: cartArray })
+}
+
+export const changeItemcountup = (state, itemKey) => {
+  const cartArray = [...state.cart];
+  cartArray[itemKey.cart] = {
+    ...cartArray[itemKey.cart],
+    itemCount: parseInt(cartArray[itemKey.cart].itemCount) + 1
+  }
+
+  return state.merge({ cart: cartArray });
+}
+
+export const changeItemcountdown = (state, itemKey) => {
+  const cartArray = [...state.cart];
+
+  if (parseInt(cartArray[itemKey.cart].itemCount) > 1) {
+    cartArray[itemKey.cart] = {
+      ...cartArray[itemKey.cart],
+      itemCount: parseInt(cartArray[itemKey.cart].itemCount) - 1
+    }
+  } else {
+    cartArray[itemKey.cart] = {
+      ...cartArray[itemKey.cart],
+      itemCount: 1
+    }
+  }
+
+  return state.merge({ cart: cartArray });
+}
+
+export const changeClearcart = (state) => {
+  const cartArray = [];
+  return state.merge({ cart: cartArray });
+}
+
+export const changeRemoveitem = (state, itemKey) => {
+  let cartArray = [...state.cart];
+  cartArray.splice(itemKey.cart, 1);
+  return state.merge({ cart: cartArray });
+}
+
+/* ------------- Hookup Reducers To Types ------------- */
+
+export const reducer = createReducer(INITIAL_STATE, {
+  [Types.CHANGE_CART]: changeCart,
+  [Types.CHANGE_ITEMCOUNTUP]: changeItemcountup,
+  [Types.CHANGE_ITEMCOUNTDOWN]: changeItemcountdown,
+  [Types.CHANGE_REMOVEITEM]: changeRemoveitem,
+  [Types.CHANGE_CLEARCART]: changeClearcart
+})
